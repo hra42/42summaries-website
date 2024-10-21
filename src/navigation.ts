529e2bf6536baffaf3a8f9178180df63e5e1091f
Henry Rausch <44985923+hra42@users.noreply.github.com>
2024-10-21 08:21:34 +0000

    Refactor astro.config.ts, CustomStyles.astro, LandingLayout.astro, Favicons.astro, Logo.astro, site.webmanifest, navigation.ts, and index.astro

--- src/navigation.ts
+++ src/navigation.ts
@@ -41,7 +41,13 @@ export const headerData = {
       ],
     },
   ],
-  actions: [{ text: 'Download', href: 'https://apps.apple.com/de/app/42summaries/id6736508972?l=en-GB&mt=12', target: '_blank' }],
+  actions: [
+    {
+      text: 'Download',
+      href: 'https://apps.apple.com/de/app/42summaries/id6736508972?l=en-GB&mt=12',
+      target: '_blank',
+    },
+  ],
 };
 
 export const footerData = {
@@ -61,7 +67,11 @@ export const footerData = {
   ],
   socialLinks: [
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/hra42/42Summaries' },
-    { ariaLabel: 'AppStore', icon: 'tabler:brand-appstore', href: 'https://apps.apple.com/de/app/42summaries/id6736508972?l=en-GB&mt=12' },
+    {
+      ariaLabel: 'AppStore',
+      icon: 'tabler:brand-appstore',
+      href: 'https://apps.apple.com/de/app/42summaries/id6736508972?l=en-GB&mt=12',
+    },
     { ariaLabel: 'Testflight', icon: 'tabler:test-pipe', href: 'https://testflight.apple.com/join/yP2NuC4V' },
     { ariaLabel: 'Notion', icon: 'tabler:brand-notion', href: 'https://42summaries.notion.site' },
   ],
